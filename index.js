d0060cf8db938be8ae1379db6ba7d75ddff9a402
Bryan Maynard <bsgbryan@gmail.com>
2017-07-26 00:14:38 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const fs = require('fs')
+
+const matrix = { }
+
+const buildKey = (word, i) => {
+  let limit   = word.length - 1
+  let key     = 0
+  let current = word.charCodeAt(i)
+
+  if (i > 0) {
+    key = word.charCodeAt(i - 1)
+  }
+
+  key =  key << 7
+  key += current
+  key =  key << 7
+
+  if (i < limit) {
+    key += word.charCodeAt(i + 1)
+  }
+
+  return key
+}
+
+fs.readFile('./dictionary.txt', 'utf8', (err, data) => {
+  const words = data
+    .split(' ')
+    .forEach((word) => {
+      let context = matrix
+
+      for (let i in word) {
+        i = parseInt(i, 10)
+
+        let key = buildKey(word, i)
+
+        if (typeof context[key] === 'undefined') {
+          context[key] = { }
+        }
+
+        context = context[key]
+      }
+    })
+
+  fs.writeFile('./matrix.json', JSON.stringify(matrix), (err) => {
+    if (err) {
+      console.log("D'oh!")
+    } else {
+      console.log('Matrix saved')
+
+      fs.readFile('./words.txt', 'utf8', (_, data) => {
+        const words = data
+          .replace(/\W+/g, ' ')
+          .split(' ')
+          .map((w) => w.toLowerCase())
+
+        for (let word of words) {
+          let context = matrix
+          let skip    = false
+
+          for (let w in word) {
+            w = parseInt(w, 10)
+
+            let key = buildKey(word, w)
+
+            if (typeof context[key] == 'undefined') {
+              console.log(word, 'is misspelled!')
+
+              skip = true
+
+              break
+            } else {
+              context = context[key]
+            }
+          }
+
+          context = matrix
+
+          if (skip) {
+            continue
+          }
+        }
+      })
+    }
+  })
+})
+
+// fs.readFile('./words.txt', 'utf8', (err, data) => {
+//   const words = data.replace(/\W+/g, ' ').split(' ')
+//
+//   const dictionary = words
+//     .filter((w, i) => words.indexOf(w) === i)
+//     .map((f) => f.toLowerCase())
+//
+//   fs.writeFile('./dictionary.txt', dictionary.join(' '), (err) => {
+//     if (err) {
+//       console.log('OH NOES')
+//     }
+//   })
+//
+//   console.log('dictionary', dictionary)
+// })
